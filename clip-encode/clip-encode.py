ddccc65d81e5e04f89d55d4575488a45ecf43b26
iejMac <kilianmaciej6@gmail.com>
2022-05-10 16:23:30 -0700

    clip-encode: use batches of 256 instead

--- clip-encode/clip-encode.py
+++ clip-encode/clip-encode.py
@@ -4,8 +4,11 @@ import skvideo.io as skv
 import sys
 import torch
 
+from torch.utils.data import DataLoader
 from torchvision.transforms import Compose, ToPILImage
 
+
+BS = 256
 
 #TODO: make this work with any encoder model
 def clipencode(src, dest):
@@ -17,13 +20,17 @@ def clipencode(src, dest):
   vid = skv.vread(src)
   video_embeddings = np.zeros((len(vid), 512)) # TODO: get embed size from model
 
-  with torch.no_grad():
-    for i, frame in enumerate(vid):
-      img = preprocess(frame).unsqueeze(0).to(device)
-      video_embeddings[i] = model.encode_image(img).cpu().detach().numpy()
+  proc_video = torch.zeros((len(vid), 3, 224, 224))
+  for i, frame in enumerate(vid):
+    proc_video[i] = preprocess(frame).unsqueeze(0)
+
+  # Batch up and embed video
+  ds_vid = DataLoader(proc_video, batch_size=BS)
+  for i, b in enumerate(ds_vid):
+    imgs = b.to(device)
+    video_embeddings[i*BS:(i+1)*BS] = model.encode_image(imgs).cpu().detach().numpy()
 
   np.save(dest, video_embeddings)
-
 
 if __name__ == "__main__":
   if len(sys.argv) != 3:
