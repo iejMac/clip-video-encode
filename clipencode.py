0b9960f699cdd183da1a248707e6b7fd2dabd4cf
iejMac <kilianmaciej6@gmail.com>
2022-05-09 18:06:04 -0700

    clip-encode: pass src and dest as args

--- clipencode.py
+++ clipencode.py
@@ -1,10 +1,11 @@
 import clip
 import numpy as np
 import skvideo.io as skv
+import sys
 import torch
 
 from PIL import Image
-from torchvision.transforms import Compose, ToPILImage, ToTensor
+from torchvision.transforms import Compose, ToPILImage
 
 
 #TODO: make this work with any encoder model
@@ -14,8 +15,7 @@ def clipencode(src, dest):
   model, preprocess = clip.load("ViT-B/32", device=device)
   preprocess = Compose([ToPILImage(), preprocess])
 
-  # vid = np.moveaxis(skv.vread(path), -1, 1)
-  vid = skv.vread(path)
+  vid = skv.vread(src)
   video_embeddings = np.zeros((len(vid), 512)) # TODO: get embed size from model
 
   with torch.no_grad():
@@ -26,30 +26,9 @@ def clipencode(src, dest):
   np.save(dest, video_embeddings)
 
 
-path = "video.mp4"
-device = "cuda" if torch.cuda.is_available() else "cpu"
-# clipencode(path, "video.npy")
+if __name__ == "__main__":
+  if len(sys.argv) != 3:
+    print("Usage: python clipencode.py video.mp4 embeddings.npy")
+    sys.exit(1)
 
-video_embs = torch.Tensor(np.load("video.npy")).to(device)
-
-# labels = ["a basketball hoop", "a basketball going into a basketball hoop", "a basketball missing a basketball hoop"]
-labels = ["basketball", "dog", "cat"]
-
-test = True
-if test:
-  model, preprocess = clip.load("ViT-B/32", device=device)
-
-  text = clip.tokenize(labels).to(device)
-
-  with torch.no_grad():
-    text_embs = model.encode_text(text)
-
-    video_embs = video_embs / video_embs.norm(dim=1, keepdim=True)
-    text_embs = text_embs / text_embs.norm(dim=1, keepdim=True)
-
-    logit_scale = model.logit_scale.exp()
-    logits_per_frame = logit_scale * video_embs @ text_embs.t()
-    
-    probs = logits_per_frame.softmax(dim=-1).cpu().numpy()
-
-  print("Label probs:", probs)  # prints: [[0.9927937  0.00421068 0.00299572]]
+  clipencode(sys.argv[1], sys.argv[2])
