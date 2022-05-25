bde034f5b2f7f6d68287c82c636d2ffd4f3f88a9
iejMac <kilianmaciej6@gmail.com>
2022-05-25 20:50:39 +0200

    clip-video-encode: add support for many videos at once

--- clip_video_encode/clip_video_encode.py
+++ clip_video_encode/clip_video_encode.py
@@ -15,59 +15,75 @@ EMB_DIM = 512
 QUALITY = "360p"
 
 
-def clip_video_encode(src, dest):
+def clip_video_encode(src, dest=None):
     """
     Encode frames using CLIP image encoder
 
     Input:
-      src: path to mp4 file
-      output: where to save embeddings.npy
+      src:
+        str: path to mp4 file
+        str: youtube link
+        str: path to txt file with multiple mp4's or youtube links
+        list: list with multiple mp4's or youtube links
+      dest:
+        str: where to save embeddings
+        None: dst = src + .npy
+
     Output:
       None
     """
-    if src.endswith(".mp4"):  # mp4 file
-        fname = src
-    else:  # youtube link
-        ydl_opts = {}
-        ydl = youtube_dl.YoutubeDL(ydl_opts)
-        formats = ydl.extract_info(src, download=False).get("formats", None)
-        f = None
-        for f in formats:
-            if f.get("format_note", None) != QUALITY:
-                continue
-            break
-        fname = f.get("url", None)
-
-    cap = cv2.VideoCapture(fname)  # pylint: disable=I1101
-    if not cap.isOpened():
-        print("Error: Video not opened")
-        sys.exit(1)
+    if isinstance(src, str):
+        if src.endswith(".txt"):  # list of mp4s or youtube links
+            with open(src, "r", encoding="utf-8") as f:
+                fnames = [fn[:-1] for fn in f.readlines()]
+        else:  # mp4 or youtube link
+            fnames = [src]
+    else:
+        fnames = src
 
     # Initialize model:
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model, preprocess = clip.load("ViT-B/32", device=device)
     preprocess = Compose([ToPILImage(), preprocess])
 
-    fc = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))  # pylint: disable=I1101
-    video_embeddings = np.zeros((fc, EMB_DIM))
-    batch = []
+    for fname in fnames:
+        if not fname.endswith(".mp4"):  # youtube link
+            ydl_opts = {}
+            ydl = youtube_dl.YoutubeDL(ydl_opts)
+            formats = ydl.extract_info(fname, download=False).get("formats", None)
+            f = None
+            for f in formats:
+                if f.get("format_note", None) != QUALITY:
+                    continue
+                break
+            fname = f.get("url", None)
 
-    ret = True
-    counter = 0
-    while ret:
-        ret, frame = cap.read()
+        cap = cv2.VideoCapture(fname)  # pylint: disable=I1101
+        if not cap.isOpened():
+            print("Error: Video not opened")
+            sys.exit(1)
 
-        if (len(batch) == BS) or ((not ret) and (len(batch) > 0)):  # encode
-            t_batch = torch.stack(batch).to(device)
-            video_embeddings[counter : counter + len(batch)] = model.encode_image(t_batch).cpu().detach().numpy()
-            counter += len(batch)
-            batch = []
+        fc = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))  # pylint: disable=I1101
+        video_embeddings = np.zeros((fc, EMB_DIM))
+        batch = []
 
-        if ret:
-            batch.append(preprocess(frame))
+        ret = True
+        counter = 0
+        while ret:
+            ret, frame = cap.read()
 
-    video_embeddings = video_embeddings[:counter]
-    np.save(dest, video_embeddings)
+            if (len(batch) == BS) or ((not ret) and (len(batch) > 0)):  # encode
+                t_batch = torch.stack(batch).to(device)
+                video_embeddings[counter : counter + len(batch)] = model.encode_image(t_batch).cpu().detach().numpy()
+                counter += len(batch)
+                batch = []
+
+            if ret:
+                batch.append(preprocess(frame))
+
+        video_embeddings = video_embeddings[:counter]
+        dst = fname[:-4] + ".npy" if dest is None else dest
+        np.save(dst, video_embeddings)
 
 
 if __name__ == "__main__":
