3ff66ea46393183e30d66fc29fb6294a2c7e01e8
iejMac <kilianmaciej6@gmail.com>
2022-06-04 02:38:31 +0200

    add modules to package

--- clip_video_encode/__init__.py
+++ clip_video_encode/__init__.py
@@ -1,3 +1,8 @@
 """clip video encode"""
 
 from .clip_video_encode import clip_video_encode
+
+from .reader import VideoReader
+from .batcher import FrameBatcher
+from .simplemapper import FrameMapper
+from .simplewriter import EmbeddingWriter
