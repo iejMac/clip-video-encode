7b0cb770b26254e4ef0cacc927af312c4460e457
iejMac <kilianmaciej6@gmail.com>
2022-08-09 17:15:37 +0000

    repl simple writer'

--- clip_video_encode/writer.py
+++ clip_video_encode/writer.py
@@ -1,15 +1,27 @@
-"""save embeddings block using vid_inds dict"""
+"""save embeddings."""
 import os
 
+import fsspec
 import numpy as np
 
 
 class FileWriter:
-    def __init__(self):
-        pass
+    """Writes output as files."""
+    def __init__(self, dest_dir):
+        self.dest_dir = dest_dir
+
+        self.fs, self.dest_dir = fsspec.core.url_to_fs(dest_dir)
+
+    def write(self, arr, dst_name):
+        save_pth = os.path.join(self.dest_dir, dst_name)
+        with self.fs.open(save_pth, "wb") as f:
+            nbp = BytesIO()
+            np.save(nbp, arr)
+            f.write(nbp.getbuffer())
 
 
 class WebDatasetWriter:
+    """Writes output in WebDataset format."""
     def __init__(self):
         pass
 
