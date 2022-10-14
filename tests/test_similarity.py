5c21457e5cebed745b0f0755d76f6850079cf41d
iejMac <kilianmaciej6@gmail.com>
2022-10-14 16:19:05 +0000

    works

--- tests/test_similarity.py
+++ tests/test_similarity.py
@@ -8,7 +8,7 @@ import open_clip
 from clip_video_encode import clip_video_encode
 
 
-def test_similarity(video):
+def test_similarity():
     test_path = "tests/test_videos"
     with tempfile.TemporaryDirectory() as tmpdir:
         clip_video_encode(
@@ -17,18 +17,17 @@ def test_similarity(video):
             output_format="files",
             take_every_nth=2,
             frame_memory_size=0.125,
-            metadata_columns=["caption", "meta"],
             use_dst_name=True,
         )
 
         model, _, _ = open_clip.create_model_and_transforms("ViT-B-32", pretrained="laion2b_s34b_b79k")
         text = open_clip.tokenize(["bears", "monkey"])
         with torch.no_grad(), torch.cuda.amp.autocast():
-            text_feat = model.encode_text(text)
+            text_feat = model.encode_text(text).float()
 
             for vid in ["vid1.mp4", "vid2.mp4"]:
                 frame_embeddings = np.load(os.path.join(tmpdir, vid[:-4] + ".npy"))
-                frame_feat = torch.from_numpy(frame_embeddings[0])  # only take first frame
+                frame_feat = torch.from_numpy(frame_embeddings[0]).float()  # only take first frame
 
                 text_probs = (100.0 * frame_feat @ text_feat.T).softmax(dim=-1)
 
