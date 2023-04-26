30a4cff6e90242f78e300528538b24f7f7f73634
iejmac <iejmac@ip-26-0-151-112.us-west-2.compute.internal>
2023-04-26 07:26:07 +0000

    captioning: allow setting params

--- clip_video_encode/simplemapper.py
+++ clip_video_encode/simplemapper.py
@@ -25,7 +25,25 @@ class FrameMapper:
 
     def generate_captions(self, batch):
         # TODO: idk if this is the best way to do it but works for now
+
+        # jprompt = "a video of "
+        prompt = ""
+        tok = self.tokenizer(prompt)
+        index = torch.argmax((tok == 49407).type(torch.int64))
+        tok = tok[:, :index]
+        tok = torch.cat([tok]*batch.shape[0])
+        tok = tok.to(batch.device)
+
         with torch.no_grad(), torch.cuda.amp.autocast():
-            generated = self.model.generate(batch)
-        captions = [open_clip.decode(gen).split("<end_of_text>")[0].replace("<start_of_text>", "") for gen in generated]
+            generated = self.model.generate(
+                batch,
+                text=tok,
+                generation_type="beam_search",
+                temperature=1.0,
+                top_p=0.1,
+                min_seq_len=15,
+                num_beams=10,
+                num_beam_groups=5,
+            )
+        captions = [open_clip.decode(gen).split("<end_of_text>")[0].replace("<start_of_text>", "")[len(prompt):] for gen in generated]
         return captions
